a402767901bd4d26cd36c590d9d100563fbd96c0
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-01-06 20:22:44 +0100

    Update signIn_screen.dart

--- lib/screens/signIn_screen.dart
+++ lib/screens/signIn_screen.dart
@@ -27,7 +27,7 @@ class _SignUpState extends State<SignUp> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        title: Text("Sign Upin irnfeofjiowjfiojrofweijfiowrejfiorjhfiowjfijfw Screen"),
+        title: Text("Sign Up in irnfeofjiowjfiojrofweijfiowrejfiorjhfiowjfijfw Screen"),
       ),
       body: Form(
         key: _formKey,
